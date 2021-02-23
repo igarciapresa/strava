d11f4659004aee43f16b9fabe6a7c6be2a56bdbe
igarciapresa <igarciapresa@gmail.com>
2021-02-23 19:14:59 +0100

    adding vuetify dependencies

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,6 +1,8 @@
 require('./bootstrap');
 
 window.Vue = require('vue').default;
+import Vuetify from 'vuetify';
+Vue.use(Vuetify);
 
 Vue.component('example-component', require('./components/ExampleComponent.vue').default);
 Vue.component('my-trials', require('./components/MyTrialsComponent.vue').default);
